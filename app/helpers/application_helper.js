ea987137500ecd065836a2a951682a5f95c06416
Alberto Souza <alberto.souza.99@gmail.com>
2013-07-24 01:18:18 -0300

    console.log removed from application_helper

--- app/helpers/application_helper.js
+++ app/helpers/application_helper.js
@@ -1,6 +1,5 @@
 module.exports = { 
     isLogedIn: function () {
-        console.log(this.viewContext.request.user);
         if(this.viewContext.request.user){
             return true;
         }else{
